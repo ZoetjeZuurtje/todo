22051f20e09a8c91aa77b6ae14d8bcb3f211e9fd
Bram <bramstrik@live.com>
2024-06-25 14:44:26 +0200

    Intoduce bound checking and simplify JS

--- src/components/project-card/project-card.js
+++ src/components/project-card/project-card.js
@@ -1,31 +1,36 @@
 const main = document.querySelector('main');
 
+const clamp = (min, value, max) => Math.max(Math.min(max, value), min);
+
 const dropCard = (event) => {
-    const {x, y} = main.getBoundingClientRect();
-    console.log(x, y)
-    const widthOffset = event.target.getBoundingClientRect().width / 2;
+    const cardArea = main.getBoundingClientRect();
+
+    const {height: cardHeight, width: cardWidth} = event.target.getBoundingClientRect();
     const headerOffset = event.target.querySelector('.project-drag-button').getBoundingClientRect().height / 2;
-    const totalXOffset = x + widthOffset;
-    const totalYOffset = y + headerOffset;
+    const totalXOffset = cardArea.x + cardWidth / 2;
+    const totalYOffset = cardArea.y + headerOffset;
 
-    const newX = Math.max(event.clientX - totalXOffset, 0);
-    const newY = Math.max(event.clientY - totalYOffset, 0);
-
+    // Prevent the card from being dropped to the left or above the `main` element
+    const newX = clamp(0, event.clientX - totalXOffset, cardArea.right - cardWidth - cardArea.x);
+    const newY = clamp(0, event.clientY - totalYOffset, cardArea.bottom - cardHeight - cardArea.y);
+    // Prevent the card from being dropped under or to the right of the `main` element
     event.target.style.left = `${newX}px`;
     event.target.style.top  = `${newY}px`;
-
 }
 
 const dragCard = (event) => {
-    if (event.explicitOriginalTarget.textContent != ":::::") {
+    const isDragButton = event.explicitOriginalTarget.parentElement.classList.contains('project-drag-button');
+    if (!isDragButton) {
         event.preventDefault();
     }
 }
 
 const init = () => {
-    main.addEventListener('dragend', dropCard);
+    // Prevent the 'return animation' of the draggable object
+    document.addEventListener('dragover', function(e) { e.preventDefault() });
     let cards = document.querySelectorAll('.project-card');
     for (card of cards) {
+        card.addEventListener('dragend', dropCard);
         card.addEventListener('dragstart', dragCard);
     }
 };
