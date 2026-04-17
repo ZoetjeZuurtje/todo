0cea9ff138416833e0562181a094a36eb41cf654
Bram <bramstrik@live.com>
2026-04-17 14:23:32 +0200

    Fix: Prevent this.projects from being uninitialized

--- src/projectManager.js
+++ src/projectManager.js
@@ -1,107 +1,109 @@
 import TodoItem from "./todoItem";
 import Project from "./project";
 
-
 class ProjectManager {
-    constructor(...projects) {
-        if (projects.length < 1) {
-            this.load();
-        } else {
-            for (project in projects) {
-                this.projects.push(project);
-            }
-        }
-    }
-
-    find(id) {
-        for (let i = 0; i < this.projects.length; i++) {
-            if (this.projects[i].id == id) {
-                return this.projects[i];
-            }
-        }
-        throw new Error('Project not found');
-    }
-
-    add(project) {
+  constructor(...projects) {
+    if (projects.length < 1) {
+      this.load();
+    } else {
+      for (project in projects) {
         this.projects.push(project);
-        
-        this.save();
+      }
+    }
+  }
+
+  find(id) {
+    for (let i = 0; i < this.projects.length; i++) {
+      if (this.projects[i].id == id) {
+        return this.projects[i];
+      }
+    }
+    throw new Error("Project not found");
+  }
+
+  add(project) {
+    this.projects.push(project);
+
+    this.save();
+  }
+
+  remove(id) {
+    const index = this.getIndex(id);
+    if (index === false) return;
+
+    const firstHalf = this.projects.splice(0, index);
+    const secondHalf = this.projects.splice(index + 1);
+
+    this.projects = [].concat(firstHalf, secondHalf);
+
+    this.save();
+  }
+
+  getIndex(id) {
+    for (let i = 0; i < this.projects.length; i++) {
+      if (this.projects[i].id == id) {
+        return i;
+      }
+    }
+    return false;
+  }
+
+  save() {
+    let savedProjects = [];
+    for (let i = 0; i < this.projects.length; i++) {
+      const projectJSON = this.projects[i].toJSON();
+      console.log(projectJSON);
+      savedProjects.push(projectJSON);
     }
 
-    remove(id) {
-        const index = this.getIndex(id);
-        if (index === false) return;
+    localStorage.setItem("projects", JSON.stringify(savedProjects));
+    console.log(`Final save: ${savedProjects}`);
+  }
 
-        const firstHalf = this.projects.splice(0, index);
-        const secondHalf = this.projects.splice(index + 1);
-        
-        this.projects = [].concat(firstHalf, secondHalf);
-        
-        this.save();
+  load() {
+    const STORAGE_KEY = "projects";
+    const json = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (json === null) {
+      this.clearProjects();
+      return;
     }
 
-    getIndex(id) {
-        for (let i = 0; i < this.projects.length; i++) {
-            if (this.projects[i].id == id) {
-                return i;
-            }
-        }
-        return false;
+    this.clearProjects();
+
+    for (let i = 0; i < json.length; i++) {
+      let loadedProject = this.loadProject(json[i]);
+      this.add(loadedProject);
+    }
+  }
+
+  loadProject(json) {
+    let todoList = [];
+    for (let jsonTodo in json.todoList) {
+      const todo = new TodoItem(
+        jsonTodo.title,
+        jsonTodo.description,
+        jsonTodo.dueDate,
+        jsonTodo.priority,
+        jsonTodo.notes,
+        jsonTodo.checklist,
+        jsonTodo.id,
+      );
+      todoList.push(todo);
     }
 
-    save() {
-        let savedProjects = []
-        for (let i = 0; i < this.projects.length; i++) {
-            const projectJSON = this.projects[i].toJSON();
-            console.log(projectJSON);
-            savedProjects.push(projectJSON);
-        }
+    const project = new Project(
+      json.projectName,
+      json.description,
+      todoList,
+      json.sortingSetting,
+    );
 
-        localStorage.setItem('projects', JSON.stringify(savedProjects));
-        console.log(`Final save: ${savedProjects}`);
-    }
+    return project;
+  }
 
-    load() {
-        const STORAGE_KEY = 'projects';
-        const json = JSON.parse(localStorage.getItem(STORAGE_KEY));
-        if (json === null) return;
-
-        this.clearProjects();
-
-        for (let i = 0; i < json.length; i++) {
-            let loadedProject = this.loadProject(json[i]);
-            this.add(loadedProject);
-        }
-    }
-
-    loadProject(json) {
-        let todoList = [];
-        for (let jsonTodo in json.todoList) {
-            const todo = new TodoItem(
-                jsonTodo.title,
-                jsonTodo.description,
-                jsonTodo.dueDate,
-                jsonTodo.priority,
-                jsonTodo.notes,
-                jsonTodo.checklist,
-                jsonTodo.id
-            );
-            todoList.push(todo)
-        }
-
-        const project = new Project(
-            json.projectName,
-            json.description,
-            todoList,
-            json.sortingSetting
-        );
-
-        return project;
-    }
-
-    clearProjects() {
-        this.projects = [];
-    }
+  clearProjects() {
+    this.projects = [];
+  }
 }
 
 export default ProjectManager;
